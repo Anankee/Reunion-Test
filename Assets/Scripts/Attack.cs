96797b8ecac8202fa06765ccd2f635268c86e2e9
PC <PC@DESKTOP-T1ELAVM>
2019-05-16 19:30:27 +0200

    Test1
    Create new project

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour
+{
+    public Button attackBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackBtn.onClick.AddListener(OnAttack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAttack()
+    {        
+        Debug.Log("Atack");
+    }
+}
